100c80a1b22fa4701080ea8036ce0979aa578f17
CodexWorld <nityamaity87@gmail.com>
2016-06-25 00:52:09 +0530

    Update paypallib_config.php

--- paypallib_config.php
+++ paypallib_config.php
@@ -7,6 +7,9 @@
 // Use PayPal on Sandbox or Live
 $config['sandbox'] = TRUE; // FALSE for live environment
 
+// PayPal Business Email ID
+$config['business'] = 'info@codexworld.com';
+
 // If (and where) to log ipn to file
 $config['paypal_lib_ipn_log_file'] = BASEPATH . 'logs/paypal_ipn.log';
 $config['paypal_lib_ipn_log'] = TRUE;
